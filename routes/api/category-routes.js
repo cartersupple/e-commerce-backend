e044b6ed56587f972392cef442e1ef8a0dce0f68
Carter Supple <Cartersupple123@gmail.com>
2022-01-27 19:34:52 -0800

    added category-routes post, put, and delete.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -32,18 +32,75 @@ router.get('/:id', (req, res) => {
     }
   ]
   })
+  .then(dbCategoryData => {
+    if (!dbCategoryData) {
+      res.status(404).json({
+        message: 'ID incorrect, no category found'
+      })
+      return;
+    }
+    res.json(dbCategoryData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name
+  })
+    .then(dbCategoryData => res.json(dbCategoryData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+  });
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update({
+    category_name: req.body.category_name},
+    {
+    where: {
+       id: req.params.id,
+    }
+    })
+    .then(dbCategoryData => {
+        if (!dbCategoryData[0]) {
+            res.status(404).json({ message: 'ID incorrect, no category found'});
+            return;
+        }
+        res.json(dbCategoryData);
+  })
+    .catch(err => {
+        console.log(err); 
+        res.status(500).json(err);
+  });
+
 });
+
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+        id: req.params.id
+    }
+  })
+    .then(dbCategoryData => {
+        if (!dbCategoryData) {
+            res.status(404).json({ message: 'ID incorrect, no category found'});
+            return;
+        }
+        res.json(dbCategoryData);
+  })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+  });
 });
 
 module.exports = router;
